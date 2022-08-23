ed5210754d72e93cc4cb975ca575dd06df760d43
TheSwordOfGod <3487675116qq.com>
2022-08-23 12:45:11 +0800

    第二次创建项目

---
+++ src/main/java/com/mga/test/MyTest.java
@@ -0,0 +1,13 @@
+package com.mga.test;
+
+/**
+ * @Author Mga
+ * @Date 2022/8/23 12:44
+ * @注释
+ */
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
